79807486c527f256c54240506625264ebfd87484
Ada <ajli@smith.edu>
2024-09-19 11:10:24 -0400

    Removed constructor and kept attributes

--- Conversation.java
+++ Conversation.java
@@ -2,9 +2,7 @@ public class Conversation {
   //Attributes
   String input;
   int rounds;
-  public Conversation(int rounds){
-    this.rounds = rounds;
-  }
+ 
   public static void main(String[] arguments) {
     // You will start the conversation here.
   }
