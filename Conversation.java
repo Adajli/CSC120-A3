79ebc8ae61be68fb480f92a2b892a5043f24b05d
Ada <ajli@smith.edu>
2024-09-19 11:09:52 -0400

    Set up constructor for Conversation.

--- Conversation.java
+++ Conversation.java
@@ -1,5 +1,10 @@
-class Conversation {
-
+public class Conversation {
+  //Attributes
+  String input;
+  int rounds;
+  public Conversation(int rounds){
+    this.rounds = rounds;
+  }
   public static void main(String[] arguments) {
     // You will start the conversation here.
   }
