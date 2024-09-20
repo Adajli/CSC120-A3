c6e6e9c71aa48108f17de49dc2e5c679f477bb77
Ada <ajli@smith.edu>
2024-09-20 12:40:09 -0400

    Added a line to end the converstation.

--- Conversation.java
+++ Conversation.java
@@ -4,15 +4,15 @@ public class Conversation {
   int rounds;
  
   public static void main(String[] arguments) {
-    Scanner scan = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     
     // You will start the conversation here.
     System.out.println("How many rounds would you like??");
-    int rounds = scan.nextInt(); //asks for number of rounds  
-    scan.nextLine();
-    for(int i = 0; i<rounds;i++){
+    int rounds = input.nextInt(); //asks for number of rounds  
+    input.nextLine();
+    for(int i = 0; i<=rounds;i++){
 
     }
-    
+    System.out.println("I was so delighted to talk to you.");
   }
 }
