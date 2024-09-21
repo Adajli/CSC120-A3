87efc9725b548376f15b03b457ec907bbc95155f
Ada <ajli@smith.edu>
2024-09-21 15:28:28 -0400

    Fixed inner part of for loop.

--- Conversation.java
+++ Conversation.java
@@ -5,21 +5,25 @@ public class Conversation {
   boolean match;
   public static void main(String[] arguments) {
     Scanner input = new Scanner(System.in);
-    String [][] mirrorWords = {{"I", "you"} ,{"am", "are"},{ "my", "your"}, {"me", "you"},{"you'd", "I'd"}};
+    String [][] mirrorWords = {{"i", "you"} ,{"am", "are"},{ "my", "your"}, {"me", "you"},{"i'd", "you'd"}};
     String [] cannedResponses = {"Interesting", "Mm-hm", "Can you talk more about that?", "Oh really?"};
     // You will start the conversation here.
     System.out.println("How many rounds would you like??");
     int rounds = input.nextInt(); //asks for number of rounds
-    boolean match = false;  
+    
     input.nextLine();
     System.out.println("Hello, what's on your mind?");
     for(int i = 0; i<rounds;i++){
-      String response = input.next(); //asks for response
-      input.nextLine();
+      String response = input.nextLine(); //asks for response
+      boolean match = false;  
       for(int j = 0; j < mirrorWords.length; j++){
         for(int k = 0; k < mirrorWords[j].length; k++){
           if(response.toLowerCase().contains(mirrorWords[j][k])){
-            String out = mirrorWords[j][k] + " "+ response;
+            int spaceIndex = response.indexOf(" ");
+            String out = mirrorWords[j][k];
+            if (spaceIndex >= 0) {
+                out += response.substring(spaceIndex); // Append the rest of the response after the space
+            }
             match = true;
             System.out.println(out);
             break;
@@ -28,13 +32,13 @@ public class Conversation {
         if(match==true){
           break;
         }
-      }   
+      } 
+      if(!match){
+        int responseIndex = (int) (Math.random()*cannedResponses.length);
+        System.out.println(cannedResponses[responseIndex]);
+      }
+      
     }
-    
-    if(!match){
-      int responseIndex = (int) (Math.random()*cannedResponses.length);
-      System.out.println(cannedResponses[responseIndex]);
-    }
-    System.out.println("I was so delighted to talk to you.");
+    System.out.println("I was so delighted to talk to you.");  
   }
 }
