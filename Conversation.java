c978af99730543081238feb1b74b5b24d51b44d6
Ada <ajli@smith.edu>
2024-09-21 15:15:03 -0400

    Added nested for loop and if conditions for chatbot function and reflection.

--- Conversation.java
+++ Conversation.java
@@ -2,20 +2,38 @@ import java.util.Scanner;
 public class Conversation {
   //Attributes
   int rounds;
- 
+  boolean match;
   public static void main(String[] arguments) {
     Scanner input = new Scanner(System.in);
-    
+    String [][] mirrorWords = {{"I", "you"} ,{"am", "are"},{ "my", "your"}, {"me", "you"},{"you'd", "I'd"}};
+    String [] cannedResponses = {"Interesting", "Mm-hm", "Can you talk more about that?", "Oh really?"};
     // You will start the conversation here.
     System.out.println("How many rounds would you like??");
-    int rounds = input.nextInt(); //asks for number of rounds  
+    int rounds = input.nextInt(); //asks for number of rounds
+    boolean match = false;  
     input.nextLine();
     System.out.println("Hello, what's on your mind?");
     for(int i = 0; i<rounds;i++){
       String response = input.next(); //asks for response
       input.nextLine();
-      
-        
+      for(int j = 0; j < mirrorWords.length; j++){
+        for(int k = 0; k < mirrorWords[j].length; k++){
+          if(response.toLowerCase().contains(mirrorWords[j][k])){
+            String out = mirrorWords[j][k] + " "+ response;
+            match = true;
+            System.out.println(out);
+            break;
+          }
+        }
+        if(match==true){
+          break;
+        }
+      }   
+    }
+    
+    if(!match){
+      int responseIndex = (int) (Math.random()*cannedResponses.length);
+      System.out.println(cannedResponses[responseIndex]);
     }
     System.out.println("I was so delighted to talk to you.");
   }
