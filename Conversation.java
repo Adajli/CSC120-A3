5ecb78ad75961f52b85d8f29544a2c0c37cbd595
Ada <ajli@smith.edu>
2024-09-22 07:24:20 -0400

    Set up out to be different and changed position of printing output.

--- Conversation.java
+++ Conversation.java
@@ -10,24 +10,25 @@ public class Conversation {
     // You will start the conversation here.
     System.out.println("How many rounds would you like??");
     int rounds = input.nextInt(); //asks for number of rounds
-    
+    String out = "";
     input.nextLine();
     System.out.println("Hello, what's on your mind?");// Asks intro question.
-    for(int i = 0; i<rounds;i++){
+    for(int i = 0; i<=rounds;i++){
       String response = input.nextLine(); //asks for response
       boolean match = false;  
       for(int j = 0; j < mirrorWords.length; j++){ //Loops through array to check for mirror words.
         for(int k = 0; k < mirrorWords[j].length; k++){
           if(response.toLowerCase().contains(mirrorWords[j][k])){
             int spaceIndex = response.indexOf(" ");
-            String out = mirrorWords[j][k].toUpperCase();
+              out += mirrorWords[j][k+1];
             if (spaceIndex >= 0) {
               out += response.substring(spaceIndex); // Appends the rest of the response after the space
             }
             match = true;
-            System.out.println(out); //Prints output of line.
+            System.out.println(out); //Prints output of line.              
           }
-        }   
+        }
+       
       } 
       if(!match){
         int responseIndex = (int) (Math.random()*cannedResponses.length);
@@ -35,6 +36,7 @@ public class Conversation {
       }
       
     }
+
     System.out.println("I was so delighted to talk to you."); // Ends conversation. 
   }
 }
