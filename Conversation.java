418041bd7da396b977a798ce8c52636e4da86ac1
Ada <ajli@smith.edu>
2024-09-22 20:16:33 -0400

    Updated program, rubric, and reflection.

--- Conversation.java
+++ Conversation.java
@@ -9,42 +9,66 @@ public class Conversation {
   public static void main(String[] arguments) {
     Scanner input = new Scanner(System.in);
     String [] cannedResponses = {"Interesting", "Mm-hm", "Can you talk more about that?", "Oh really?"};
+    String transcript = "";
     // You will start the conversation here.
     System.out.println("How many rounds would you like?? ");
+    transcript += "How many rounds would you like?? ";
     int rounds = input.nextInt(); //asks for number of rounds
-    input.nextLine();
+    transcript += rounds;
     System.out.println("Hello, what's on your mind?");// Asks intro question.
+    transcript +="Hello, what's on your mind?";
+    Scanner input2 = new Scanner(System.in);
     for(int i = 0; i<rounds;i++){
-      String response = input.nextLine(); //asks for response
-      if(response.contains("Nothing")){
+      String response = input2.nextLine(); //asks for response
+      
+      //System.out.println("In loop");
+      //String response = input.nextLine(); //asks for response
+      //System.out.println("Response: "+response);  
+      //System.out.println("Response2: "+mirror(response));   
+      String response2 = mirror(response);
+      transcript +=response2;
+      //String response2 = "m";
+      
+      if(response2.equals(response)){
         int responseIndex = (int) (Math.random()*cannedResponses.length);
         System.out.println(cannedResponses[responseIndex]); 
-      }else{
+        transcript +=cannedResponses[responseIndex];
+      }else{   
         System.out.println(mirror(response));
-      }
+        transcript += mirror(response);
       } 
-      //System.out.println(out);   
-      System.out.println("I was so delighted to talk to you."); // Ends conversation. 
     }
+    System.out.println(transcript);
+    input.close();
+    input2.close();
+
+       
+    //System.out.println(out);   
+    System.out.println("I was so delighted to talk to you."); // Ends conversation. 
+  }
 
     public static String mirror(String response){
-      String [][] mirrorWords = {{"i", "you"} ,{"am", "are"},{ "my", "your"}, {"me", "you"},{"i'm", "you're"}, {".", "?"} };
+      String [][] mirrorWords = {{"i", "you"} ,{"am", "are"},{ "my", "your"}, {"me", "you"},{"i'm", "you're"} };
       String [] out = response.split(" ");
 
       for(int l = 0; l<out.length;l++){ //Runs through response variable.
         for(int j = 0; j<mirrorWords.length;j++){
-          System.out.println(out[l].toLowerCase());
+          //System.out.println(out[l].toLowerCase());
             if(out[l].toLowerCase().equals(mirrorWords[j][0])){
               out[l] = mirrorWords[j][1];
             }         
           } 
        }
-      System.out.println(Arrays.toString(out));
-      String outResult = " ";
+      
+      String outResult = "";
       for(int k = 0; k<out.length;k++){
-        outResult += out[k] + " ";
+        if(k== out.length-1){
+          outResult += out[k];
+        }else{
+          outResult += out[k] + " ";
+        }
       }
-      return outResult.trim();
+      return outResult;
     }
 } 
     
--- reflection.md
+++ reflection.md
@@ -3,6 +3,7 @@ Use this file to record your reflection on this assignment.
 - Which classmates did you consult with while working on this assignment?
   I consulted Mariam while working on this assignment.
 - Which session(s) of TA / office hours did you attend?
+  I attended the Sunday TA hours from 1-3 pm and 7-9 pm.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
