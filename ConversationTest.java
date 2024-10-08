f23219b677e64d3ac0a35bc3bf2178044fbcbb41
Ada <ajli@smith.edu>
2024-10-08 15:48:27 -0400

    Modified conversation.java and ConversationTest.java

--- ConversationTest.java
+++ ConversationTest.java
@@ -2,8 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*; 
 public class ConversationTest { 
     @Test 
-    public void TestSomeMethod() {  
-        Conversation.main(String[]arguments);
+    public void testMain() {  
+        Conversation c = new Conversation();
+        String response = "I am telling you that I am happy";
+        assertEquals("You are telling me that you are happy",c.mirror(response));
 
     } 
 }
